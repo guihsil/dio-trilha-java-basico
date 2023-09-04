f154a1f7a2bafb560b1afa196b9dd9e6a6c363ff
Guilherme Oliveira <guilherme1234oliveira.go@gmail.com>
2023-09-04 13:32:49 -0300

    atualizacao de codigo para conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, insira o número da sua conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número de sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("por favor, informe seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Informe seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+nome+" obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" conta "+numero+" e seu saldo "+saldo+" já está disponível para saque!!");
     }
 }
